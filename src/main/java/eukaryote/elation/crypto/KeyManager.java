bc9c2d057482979be73644828baed798e80aa6e0
eukaryote <minerguy31@gmail.com>
2017-10-27 18:14:40 -0600

    Fix KeyManager public key file reading

--- src/main/java/eukaryote/elation/crypto/KeyManager.java
+++ src/main/java/eukaryote/elation/crypto/KeyManager.java
@@ -9,11 +9,12 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateCrtKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 
 import org.apache.commons.io.FileUtils;
-
 
 public class KeyManager {
 	private KeyPair pair;
@@ -23,22 +24,24 @@ public class KeyManager {
 	public KeyManager() throws NoSuchAlgorithmException, NoSuchProviderException {
 		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
 		keyGen.initialize(2048);
-		
+
 		this.pair = keyGen.generateKeyPair();
 		this.privateKey = pair.getPrivate();
 		this.publicKey = pair.getPublic();
 	}
-	
+
 	public KeyManager(File pkeyfile) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
 		byte[] pkeybytes = FileUtils.readFileToByteArray(pkeyfile);
 		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(pkeybytes);
 		KeyFactory kf = KeyFactory.getInstance("RSA");
 		
+		
 		// read private key
 		this.privateKey = kf.generatePrivate(spec);
-		
+
 		// read public key
-		this.publicKey = kf.generatePublic(spec);
+		RSAPrivateCrtKey rsapub = (RSAPrivateCrtKey) privateKey;
+		this.publicKey = kf.generatePublic(new RSAPublicKeySpec(rsapub.getModulus(), rsapub.getPublicExponent()));
 	}
 
 	public PrivateKey getPrivateKey() {
--- src/test/java/eukaryote/elation/crypto/TestKeyManager.java
+++ src/test/java/eukaryote/elation/crypto/TestKeyManager.java
@@ -1,7 +1,12 @@
 package eukaryote.elation.crypto;
 
+import static org.junit.Assert.assertArrayEquals;
+
 import java.io.File;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.spec.InvalidKeySpecException;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -11,7 +16,7 @@ import org.junit.rules.TemporaryFolder;
 public class TestKeyManager {
 	
 	@Rule
-	TemporaryFolder tf = new TemporaryFolder();
+	public TemporaryFolder tf = new TemporaryFolder();
 	
 	File testdir;
 	
@@ -21,8 +26,18 @@ public class TestKeyManager {
 	}
 	
 	@Test
-	public void test() throws IOException {
+	public void test() throws IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException {
+		File keyfile = new File(testdir, "keyfile");
 		
+		KeyManager km = new KeyManager();
+		byte[] pubkey = km.getPublicKey().getEncoded();
+
+		km.writeToFile(keyfile);
 		
+		KeyManager km2 = new KeyManager(keyfile);
+		byte[] pubkey2 = km2.getPublicKey().getEncoded();
+		
+		// ensure public key is deterministic
+		assertArrayEquals(pubkey, pubkey2);
 	}
 }
