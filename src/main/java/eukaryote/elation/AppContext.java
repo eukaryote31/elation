a08ae2d8d2ca2052fe672c7e382ad5feb8a5611a
eukaryote <minerguy31@gmail.com>
2017-10-29 17:40:12 -0600

    Tying things together

--- src/main/java/eukaryote/elation/AppContext.java
+++ src/main/java/eukaryote/elation/AppContext.java
@@ -1,9 +1,21 @@
 package eukaryote.elation;
 
 import eukaryote.elation.db.PersistenceProvider;
+import eukaryote.elation.networking.EgressPool;
+import eukaryote.elation.networking.IngressSocket;
 import lombok.Getter;
 
 public class AppContext {
 	@Getter
 	PersistenceProvider persistenceProvider;
+	
+	@Getter
+	IngressSocket ingress;
+	
+	@Getter
+	EgressPool egress;
+	
+	public AppContext(PersistenceProvider pp) {
+		this.persistenceProvider = pp;
+	}
 }
--- src/main/java/eukaryote/elation/networking/EgressConnection.java
+++ src/main/java/eukaryote/elation/networking/EgressConnection.java
@@ -6,11 +6,17 @@ import java.net.URI;
 import com.neovisionaries.ws.client.WebSocket;
 import com.neovisionaries.ws.client.WebSocketFactory;
 
+import eukaryote.elation.networking.packet.Packet;
+
 public class EgressConnection {
 	private WebSocket websocket;
 
 	public EgressConnection(URI uri) throws IOException {
 		WebSocketFactory factory = new WebSocketFactory();
 		websocket = factory.createSocket(uri);
+	}
+	
+	public void sendPacket(Packet p) {
+		websocket.sendBinary(p.serialize());
 	}
 }
